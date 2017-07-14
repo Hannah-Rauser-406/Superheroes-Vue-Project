d18f1baa7c9a6c39f7f8fe970e2d8c14d048b408
Hannah Hendrickson <hannahhendrickson@Hannahs-MBP.PK5001Z>
2017-07-14 10:39:29 -0600

    linked villains to server

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose');
 var Superhero = require('./models/Superhero');
 //var mainRoutes = require('./routes/main');
 var heroRoutes = require('./routes/superheroes');
-//var Villain = require('.')
+var villainRoutes = require('./routes/villains');
+var Villain = require('./models/Villain');
 var app = express();
 var port= 3001;
 
@@ -19,7 +20,9 @@ app.use("/api/heroes", heroRoutes);
 
 //links the html, css, and js files
 app.use(express.static(__dirname + "/public"));
-//app.use("/api/villains", )
+
+
+app.use("/api/villains", villainRoutes);
 // req=object, parameter is a unique parameter (id, etc.,) the /:_id= find by id
 
 var server = app.listen(port, function(){
